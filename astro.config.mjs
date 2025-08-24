92929c058df1a4f4b06276bf6dc9024afac03b19
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2025-08-24 14:31:51 +0700

    setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://thedreambook.github.io',
-	integrations: [mdx(), sitemap()],
+  site: 'https://thedreambook.github.io',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
